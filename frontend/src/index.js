23103960d5cdb385e2857239c06e29b3f5eeab10
mateusz.littwin <mateusz.littwin@gforces.pl>
2019-01-17 22:33:08 +0100

    Add env file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
 const form = document.querySelector("form");
 
 const handleSubmit = () => {
   const formData = new FormData(form);
   const xhr = new XMLHttpRequest();
 
-  xhr.open("post", "http://localhost:4000/api/subscription/add", true);
+  xhr.open("post", `${process.env.API_URL}/subscription/add`, true);
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.send(
     JSON.stringify({
