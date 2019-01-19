5471729594691e49a5e73cb95c8641bc57a36bfe
Mateusz Wojcik <xmaten13@gmail.com>
2019-01-19 17:13:27 +0100

    Save emails in firebase database

--- main.js
+++ main.js
@@ -7,3 +7,27 @@ input.addEventListener('keyup', () => {
     input.classList.remove('form__input--filled');
   }
 });
+
+
+const config = {
+  apiKey: 'AIzaSyAYWMXR8w8m16F7pVp_RZsnsBg1JkjYkVY',
+  authDomain: 'academy-c7303.firebaseapp.com',
+  databaseURL: 'https://academy-c7303.firebaseio.com',
+  projectId: 'academy-c7303',
+  storageBucket: 'academy-c7303.appspot.com',
+  messagingSenderId: '587195173569',
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+const ref = database.ref('emails');
+
+const submitButton = document.querySelector('.form__button');
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const email = input.value;
+
+  ref.push(email);
+});
