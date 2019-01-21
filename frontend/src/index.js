5f060ea71b18ad4a382b91c4e141f174115a14cb
mateusz.littwin <mzastue@gmail.com>
2019-01-21 22:02:37 +0100

    Add thank you message

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,13 @@ const handleSubmit = () => {
   const formData = new FormData(form);
   const xhr = new XMLHttpRequest();
 
+  xhr.addEventListener('load', () => {
+    form.outerHTML = "<h2>Thank you for subscription.</h2> <br> You will be redirected in few seconds..";
+    setTimeout(() => {
+      window.location.href = '/';
+    }, 4000);
+  });
+
   xhr.open("post", `${process.env.API_URL}/addEmail`, true);
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.send(
