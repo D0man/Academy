8cfa32ca213d0714b59adf6d1f56fbf0d5206fee
apolios <airmoss@gmail.com>
2019-02-07 15:55:12 +0100

    added menu

--- js/app2.js
+++ js/app2.js
@@ -1,6 +1,8 @@
 const burger = document.querySelector(".burger");
 const navbtn = document.querySelector(".navbtn");
+const nav = document.querySelector(".nav");
 
 navbtn.addEventListener("click", ()=>{
-  burger.classList.toggle("open");
+  burger.classList.toggle("burger--open");
+  nav.classList.toggle("nav--open");
 });
